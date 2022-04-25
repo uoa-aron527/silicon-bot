d3d1671a2ce73ee668f43f503251ba22dc7d9bf5
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-04-26 10:03:21 +1200

    Added a few commands and a private API key

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 require('dotenv').config();
 const token = process.env.TOKEN;
 
+const prefix = "--";
+
 //const fetch = require("node-fetch");
 
 client.once("ready", () => {
-    console.log("Silicon is online!");
+    console.log("Silicon is up and running!");
 });
 
-client.on("messageCreate", async msg => {
-    if(msg.content == 'Hey'){
-    msg.reply("Yo Silico here!");
+client.on("messageCreate", msg => {
+    // If invalid prefix or a bot has sent the message, no actions
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;        
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift();
+
+    if(command == 'Hey'){
+        msg.channel.send("Yo Silicon here!");
+    }
+    else if(command == 'source'){
+        msg.channel.send("Find me at https://github.com/uoa-aron527/silicon-bot");
     }
 });
 
